394aac3039cf7d1e8b090756227440a56838ce7e
Isa Reinert <reinert.isa@gmail.com>
2025-05-25 17:31:18 -0700

    feat(website): create Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from "./components/Navbar.tsx";
+import Home from "./components/Home.tsx";
 
 function App() {
   return (
       <div className="App">
           <Navbar />
+          <Home />
       </div>
 
   )
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h2>Home page</h2>
+        </div>
+    )
+}
