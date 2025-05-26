70bc88bbdddf22bb467f326e38a754387dc3b763
Isa Reinert <reinert.isa@gmail.com>
2025-05-25 17:32:14 -0700

    feat(website): wrap home component with div

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   return (
       <div className="App">
           <Navbar />
-          <Home />
+          <div className="content">
+              <Home />
+          </div>
       </div>
 
   )
