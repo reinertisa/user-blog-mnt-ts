894655948c37b2fcfa487f27e2e73a49269ee04d
Isa Reinert <reinert.isa@gmail.com>
2025-05-25 17:19:18 -0700

    feat(website): initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from "./components/Navbar.tsx";
+
+function App() {
+  return (
+      <div className="App">
+          <Navbar />
+      </div>
+
+  )
+}
+
+export default App
