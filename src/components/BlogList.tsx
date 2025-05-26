225b1de86ec6fdae13235bad1f371e8fa0f52827
Isa Reinert <reinert.isa@gmail.com>
2025-05-26 00:57:13 -0700

    feat(website): add props

---
+++ src/components/BlogList.tsx
@@ -0,0 +1,22 @@
+import type {Blog} from "./Home.tsx";
+
+type BlogListProps = {
+    blogs: Blog[],
+    title: string,
+}
+
+export default function BlogList({blogs, title}: BlogListProps) {
+
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    )
+
+}
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
+import BlogList from "./BlogList.tsx";
 
-type Blog = {
+export type Blog = {
     title: string,
     body: string,
     author: string,
@@ -17,12 +18,7 @@ export default function Home() {
 
     return (
         <div className="home">
-            {blogs.map(blog => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs" />
         </div>
     )
 }
