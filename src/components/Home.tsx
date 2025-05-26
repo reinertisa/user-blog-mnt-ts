3a8b02592c5982cb78a34b7b292c31b827ca0a4a
Isa Reinert <reinert.isa@gmail.com>
2025-05-26 00:48:24 -0700

    feat(website): outputting lists

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,28 @@
+import {useState} from 'react';
+
+type Blog = {
+    title: string,
+    body: string,
+    author: string,
+    id: number,
+}
+
 export default function Home() {
+
+    const [blogs] = useState<Blog[]>([
+        {title: 'First Blog', body: 'Hey how are you?', author: 'Isa', id: 1},
+        {title: 'Second Blog', body: 'How old are you?', author: 'Sade', id: 2},
+        {title: 'Third Blog', body: 'Where do you come from?', author: 'Kristi', id: 3},
+    ])
+
     return (
         <div className="home">
-            <h2>Home page</h2>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     )
 }
