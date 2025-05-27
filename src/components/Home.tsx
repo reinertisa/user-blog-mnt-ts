d0066a8727dd2838fd9c2f8f20179cb87432b8af
Isa Reinert <reinert.isa@gmail.com>
2025-05-27 13:12:23 -0700

    feat(website): conditional loading message

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,23 +10,28 @@ export type Blog = {
 
 export default function Home() {
     const [blogs, setBlogs] = useState<Blog[] | null>(null)
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const loadData = async () => {
             try {
+                setLoading(true);
                 const rez = await fetch('http://localhost:8000/blogs')
                 const data = await rez.json();
                 setBlogs(data);
+                setLoading(false);
             } catch (err) {
                 console.log(err);
+                setLoading(false)
             }
         }
-        void loadData();
+        setTimeout(loadData, 1000)
     }, [])
 
 
     return (
         <div className="home">
+            {loading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     )
